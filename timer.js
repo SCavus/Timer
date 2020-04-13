535a3248ee2f17802cdeedc9db0da857683f34e3
SERKAN CAVUS <40465585+SCavus@users.noreply.github.com>
2020-04-14 00:58:22 +0100

    Create timer.js

---
+++ timer.js
@@ -0,0 +1,61 @@
+let reset = document.querySelector('#reset')
+
+function setAlarm(){
+  let inputField = document.querySelector('#alarmSet')
+  let title = document.querySelector('#timeRemaining')
+  let time = document.querySelector("#time");
+  let audioEl = document.querySelector("#myAudio")
+  let body = document.querySelector('body')
+  let set = inputField.value*1000
+  let countdown = setInterval(timer, 1000)
+
+    function timer() {
+      let min = Math.floor((set % (1000 * 60 * 60)) / (1000 * 60));
+      let sec = Math.floor((set % (1000 * 60)) / 1000);
+    
+      time.innerHTML = `${min} : ${sec}`
+      set = set-1000
+      if (set < 10000) {
+        let red = setTimeout(function () {
+          time.style.color = 'white'
+          title.style.color = 'white'
+        }, 700)
+        let white= setTimeout(function white () {
+          time.style.color = 'red'
+          title.style.color = 'red'
+        }, 300)
+        if (set < 0) {
+          clearTimeout(red)
+          clearTimeout(white)
+          body.style.background = 'red'
+          title.style.color = 'white'
+          time.style.color = 'white'
+        }
+      }
+      if (set < 0) {
+        clearInterval(countdown)
+        playAlarm(audioEl);
+      }
+    }
+}
+
+reset.addEventListener('click', ()=>{
+  location.reload(true)
+})
+
+var audio = new Audio("alarmsound.mp3");
+function setup() {
+  document.getElementById("set").addEventListener("click", () => {
+    setAlarm();
+  });
+  document.getElementById("stop").addEventListener("click", () => {
+    pauseAlarm();
+  });
+}
+function playAlarm() {
+  audio.play();
+}
+function pauseAlarm() {
+  audio.pause();
+}
+window.onload = setup
